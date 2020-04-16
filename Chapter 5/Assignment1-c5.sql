cc1f3d379666d9dde0ec5ef17ff422e62310dda5
Manuel Fontan <manuel.fontan@mongodb.com>
2020-04-16 19:33:55 +0100

    Chapter 5 assignments

---
+++ Chapter 5/Assignment1-c5.sql
@@ -0,0 +1,25 @@
+use employees;
+SELECT 
+    COUNT(*) as "Employees"
+FROM
+    employees;
+SELECT 
+    COUNT(*) as "Salaries"
+FROM
+    salaries;
+SELECT 
+    COUNT(*) as "Titles"
+FROM
+    titles;
+SELECT 
+    COUNT(*) as "Dept. Manager"
+FROM
+    dept_manager;
+SELECT 
+    COUNT(*) as "Dept. Employee"
+FROM
+    dept_emp;
+SELECT 
+    COUNT(*) AS "Departments"
+FROM
+    departments;
